454c1104819dd37e65dc3cfa9a4b787d8854adf0
dzariq mirza <dzariq85@gmail.com>
2023-10-05 13:27:23 +0800

    aa

--- app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask 
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return '<h1>Deployed!</h1><style>body { display: flex; align-items: center; justify-content: center; height: 100vh; }</style>'
